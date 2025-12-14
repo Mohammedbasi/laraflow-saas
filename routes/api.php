0afe93d0abfd845dccfe5dee4d109166cb41dfa1
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-12-14 17:26:01 +0200

    Create the Auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::middleware(['auth:sanctum', 'tenant.context'])->group(function () {
+
+            Route::get('/me', [AuthController::class, 'me']);
+
+            Route::post('/logout', [AuthController::class, 'logout']);
+
+        });
+    });
+
+    Route::middleware(['auth:sanctum', 'tenant.context'])->group(function () {
+        // Route::apiResource('projects', ProjectController::class);
+        // tasks later
+    });
+});
