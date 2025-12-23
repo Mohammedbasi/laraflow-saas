6241c35427a784911ed50456b172a07b1dfca6d2
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-12-23 14:13:36 +0200

    Create tasks request

---
+++ app/Http/Requests/Task/UpdateTaskRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use App\Models\Task;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['sometimes', 'required', 'string', 'max:255'],
+            'description' => ['sometimes', 'nullable', 'string'],
+            'status' => ['sometimes', 'required', 'string', Rule::in(Task::STATUSES)],
+            'position' => ['sometimes', 'required', 'integer', 'min:1'],
+            'due_at' => ['sometimes', 'nullable', 'date'],
+            'assignee_id' => ['sometimes', 'nullable', 'integer', 'exists:users,id'],
+        ];
+    }
+}
