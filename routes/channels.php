96a0662995e7dc43c34f7dbf05cacb3409db92a4
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-12-24 15:22:36 +0200

    Implement channel authorization

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('App.Models.Tenant.{id}', function ($user, $id) {
+
+    // Strict tenant membership check (prevents cross-tenant subscription)
+    if ((int) $user->tenant_id !== (int) $id) {
+        return false;
+    }
+
+    if (! $user->is_active) {
+        return false;
+    }
+
+    if ($user->tenant?->is_suspended) {
+        return false;
+    }
+
+     return true;
 });
