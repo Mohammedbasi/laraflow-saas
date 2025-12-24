3920d3100f7e766ee76f55e1359a1b1cccfb1253
Mohammed Zuqlam <mzuqlam@gmail.com>
2025-12-24 15:19:13 +0200

    Broadcasting auth for Sanctum tokens

---
+++ app/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\ServiceProvider;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // allow API token auth (Sanctum) and tenant context for channel auth.
+        Broadcast::routes([
+            'middleware' => ['auth:sanctum', 'tenant.context'],
+        ]);
+
+        require base_path('routes/channels.php');
+    }
+}
